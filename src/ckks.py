6dac319dc9c312a4da36889afe934863ee933bc4
Fahran Bajaj <fbajaj@terpmail.umd.edu>
2026-07-08 11:32:35 -0500

    Anderson-Darling, CKKS
    Gives a function that implements Anderson-Darling normality test for known mean, std. Also gives a short function that returns ckks encryption context with my chosen parameters

---
+++ src/ckks.py
@@ -0,0 +1,7 @@
+import tenseal as ts
+
+def generage_ckks_context() -> ts.Context:
+    """Create a new context for CKKS encryption"""
+    context = ts.context(ts.SCHEME_TYPE.CKKS, poly_modulus_degree=4096)
+    context.global_scale = pow(2, 40)
+    return context
