ea8e7fea34df957bbef9a771f9660a1294d42259
FahranBajaj <fahran.bajaj@gmail.com>
2026-07-13 09:53:36 -0500

    Memory monitor environment variable

--- src/client_app.py
+++ src/client_app.py
@@ -1,5 +1,6 @@
 import math
 import random
+import os
 from logging import ERROR, DEBUG
 
 import torch
@@ -11,6 +12,7 @@ from opacus import PrivacyEngine
 
 from src import model_loading, data_loading, util
 
+os.environ["RAY_memory_monitor_refresh_ms"] = "0"
 
 # Flower ClientApp
 app = ClientApp()
