acefeef6f39a0b3dd96ba06fd37fe19f6954b377
Fahran Bajaj <fbajaj@terpmail.umd.edu>
2026-07-23 15:06:06 -0500

    EMNIST hyperparam testing

--- src/model_loading.py
+++ src/model_loading.py
@@ -47,12 +47,32 @@ class WeatherModel(nn.Module):
         x = F.relu(self.fc1(x))
         return self.fc2(x)
 
+class EMNIST_CNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=30, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=30, out_channels=5, kernel_size=3, stride=1, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.fc1 = nn.Linear(7 * 7 * 5, 100)
+        self.fc2 = nn.Linear(100, 62)
+
+    def forward(self, x):
+        x = x.view(-1, 1, 28, 28)
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = x.view(-1, 7 * 7 * 5)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
 def model():
     with open("pyproject.toml", 'rb') as f:
         config_dict = tomllib.load(f)["tool"]["flwr"]["app"]["config"]
 
-    dataset = Datasets.WEATHER if config_dict["dataset"] == "WEATHER" else Datasets.CIFAR10 if config_dict["dataset"] == "CIFAR10" else Datasets.MNIST
-    return CIFARModel() if dataset == Datasets.CIFAR10 else WeatherModel() if dataset == Datasets.WEATHER else MNISTModel()
+    dataset = Datasets.EMNIST if config_dict["dataset"] == "EMNIST" else Datasets.WEATHER if config_dict["dataset"] == "WEATHER" else Datasets.CIFAR10 if config_dict["dataset"] == "CIFAR10" else Datasets.MNIST
+    return EMNIST_CNN() if dataset == Datasets.EMNIST else CIFARModel() if dataset == Datasets.CIFAR10 else WeatherModel() if dataset == Datasets.WEATHER else MNISTModel()
 
     
 def loss(train: bool = True):
