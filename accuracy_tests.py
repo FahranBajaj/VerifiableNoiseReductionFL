d64a544c14e1ebfeba837185f533e0917cd5f568
FahranBajaj <fahran.bajaj@gmail.com>
2026-07-24 10:18:12 -0500

    Fixed num clients for WEATHER

--- accuracy_tests.py
+++ accuracy_tests.py
@@ -8,28 +8,31 @@ for epsilon in [1,2,4,8]:
     #No dp
     config_dict["tool"]["flwr"]["app"]["config"]["use-dp"] = False
     for dataset in ["MNIST", "EMNIST", "WEATHER"]:
+        num_clients = 45 if dataset == "WEATHER" else 100
         config_dict["tool"]["flwr"]["app"]["config"]["dataset"] = dataset
         with open("pyproject.toml", 'w') as f:
             toml.dump(config_dict, f)
 
-        subprocess.run("flwr run . --stream --federation-config \"num-supernodes=100 client-resources-num-cpus=1\"", shell = True)
+        subprocess.run(f"flwr run . --stream --federation-config \"num-supernodes={num_clients} client-resources-num-cpus=1\"", shell = True)
 
     #ours
     config_dict["tool"]["flwr"]["app"]["config"]["use-dp"] = True
     config_dict["tool"]["flwr"]["app"]["config"]["noise-reduction"] = True
     for dataset in ["MNIST", "EMNIST", "WEATHER"]:
+        num_clients = 45 if dataset == "WEATHER" else 100
         config_dict["tool"]["flwr"]["app"]["config"]["dataset"] = dataset
         with open("pyproject.toml", 'w') as f:
             toml.dump(config_dict, f)
 
-        subprocess.run("flwr run . --stream --federation-config \"num-supernodes=100 client-resources-num-cpus=1\"", shell = True)
+        subprocess.run(f"flwr run . --stream --federation-config \"num-supernodes={num_clients} client-resources-num-cpus=1\"", shell = True)
 
     #no noise reduction
     config_dict["tool"]["flwr"]["app"]["config"]["use-dp"] = True
     config_dict["tool"]["flwr"]["app"]["config"]["noise-reduction"] = False
     for dataset in ["MNIST", "EMNIST", "WEATHER"]:
+        num_clients = 45 if dataset == "WEATHER" else 100
         config_dict["tool"]["flwr"]["app"]["config"]["dataset"] = dataset
         with open("pyproject.toml", 'w') as f:
             toml.dump(config_dict, f)
 
-        subprocess.run("flwr run . --stream --federation-config \"num-supernodes=100 client-resources-num-cpus=1\"", shell = True)
+        subprocess.run(f"flwr run . --stream --federation-config \"num-supernodes={num_clients} client-resources-num-cpus=1\"", shell = True)
