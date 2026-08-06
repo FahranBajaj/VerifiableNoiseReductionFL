8e348d416feb96dbd74c338a5527ba6464a31774
Fahran Bajaj <fbajaj@terpmail.umd.edu>
2026-08-06 12:34:55 -0500

    Centralized

---
+++ centralizedLearningTests/MNIST.py
@@ -0,0 +1,73 @@
+import os
+import csv
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision.datasets import MNIST
+from torchvision.transforms import Compose, Normalize, ToTensor
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+
+#load data
+batch_size = 128
+transforms = Compose([ToTensor(), Normalize((0.1307,), (0.3081,))])
+train_dataset = MNIST(root = 'data/', download = False, train = True, transform = transforms)
+test_dataset = MNIST(root = 'data/', download = False, train = False, transform = transforms)
+train_loader = DataLoader(train_dataset, batch_size, shuffle = True)
+test_loader = DataLoader(test_dataset, batch_size, shuffle = False)
+
+#define model
+class MNISTModel(nn.Module):
+    def __init__(self):
+        super(MNISTModel, self).__init__()
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(784, 100)
+        self.fc2 = nn.Linear(100, 10)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(self.flatten(x)))
+        return self.fc2(x)
+
+model = MNISTModel().to(device)
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr = 0.03)
+model.train()
+
+FILE_TO_WRITE = "centralizedLearningTests/MNIST.csv"
+fieldnames = ["update-round","test-loss","test-accuracy"]
+with open(FILE_TO_WRITE, 'a', newline = '') as csv_file:
+    writer = csv.DictWriter(csv_file, fieldnames = fieldnames)
+    writer.writeheader()
+
+NUM_EPOCHS = 100
+for epoch in tqdm(range(NUM_EPOCHS)):
+    #Train
+    for batch in train_loader:
+        optimizer.zero_grad()
+        criterion(model(batch[0].to(device)), batch[1].to(device)).backward()
+        optimizer.step()
+
+    #Evaluate
+    test_correct = 0
+    test_loss = 0.0
+    for batch in test_loader:
+        outputs = model(batch[0].to(device))
+        labels = batch[1].to(device)
+        test_loss += criterion(outputs, labels).item()
+        test_correct += (torch.max(outputs.data, 1)[1] == labels).sum().item()
+
+    test_accuracy = test_correct / len(test_loader.dataset)
+    test_loss = test_loss / len(test_loader)
+
+    results_dict = {
+        "update-round": epoch,
+        "test-loss": test_loss,
+        "test-accuracy": test_accuracy
+    }
+    with open(FILE_TO_WRITE, 'a', newline = '') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames = fieldnames)
+        writer.writerow(results_dict)    
