ce188ad0e86496abc8c1bb9427629feb88c01620
Fahran Bajaj <88906129+FahranBajaj@users.noreply.github.com>
2026-08-31 11:44:37 -0400

    Delete OldResults directory

--- OldResults/HyperParamSearch/WeatherTesting/hyperparamsearch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-clipping_norms=(1 2 0.5 4 0.25)
-for norm in ${clipping_norms[@]}
-do
-    echo "Bash script process id: $$"
-    flwr run . --stream --federation-config "num-supernodes=45 client-resources-num-cpus=1" --run-config "max-norm=${norm}"
-done
